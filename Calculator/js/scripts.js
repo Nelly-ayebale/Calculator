8968bb0789324ebeab1b14502847e5b68d8b75cc
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-08-19 15:18:12 +0300

    Made changes to the scripts.js

--- Calculator/js/scripts.js
+++ Calculator/js/scripts.js
@@ -1,11 +1,9 @@
 var add = function (number1, number2) {
     return number1 + number2;
 };
-var five = 5;
-var six = 6;
-alert((add(five, six)))
 
 var number1 = parseInt(prompt("Enter a number."));
 
 var number2 = parseInt(prompt("Enter another number."));
-alert((add(number1, number2)))
+var result = add((number1, number2))
+alert(result);
