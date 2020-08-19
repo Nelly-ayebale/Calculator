bb62cfb0cae182c6666032e4f485e69dfb789369
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-08-19 15:13:34 +0300

    Created js and css folder, index.html and link to scripts.js inside the html

---
+++ Calculator/js/scripts.js
@@ -0,0 +1,11 @@
+var add = function (number1, number2) {
+    return number1 + number2;
+};
+var five = 5;
+var six = 6;
+alert((add(five, six)))
+
+var number1 = parseInt(prompt("Enter a number."));
+
+var number2 = parseInt(prompt("Enter another number."));
+alert((add(number1, number2)))
